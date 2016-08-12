d4f5dd093286d4cb1f8fdbeb35254da0fbf9cea5
Asier Marqués <asier@MacBook-Pro-de-Asier.local>
2016-08-12 16:08:17 +0200

    first commit

---
+++ public/wp-config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+$config_dev_file  = __DIR__ . "/../config-dev.php";
+$config_prod_file = __DIR__ . "/../config-prod.php";
+
+if( file_exists( $config_prod_file ) ) {
+	include $config_prod_file;
+}else{
+	include $config_dev_file;
+}
+
+
+define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
+
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', __DIR__ . '/_wp');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
